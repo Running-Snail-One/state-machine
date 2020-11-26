c534b60154e507d1f144bd63fa78d636f6f353dc
范帅兵 <sinfan@yeah.net>
2020-11-26 23:13:04 +0800

    update .gitignore

--- src/main/java/com/demo/Application.java
+++ src/main/java/com/demo/Application.java
@@ -11,7 +11,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableAutoConfiguration
 @EnableSwagger2
 public class Application implements CommandLineRunner{
     @Autowired
